efdf050397dc12a0c5cf52f147e3674638cbeaa9
BuildTools <unconfigured@null.spigotmc.org>
2020-04-11 18:34:09 -0300

    Complete el ejercicio 1

--- Guia 1/E1/Program.cs
+++ Guia 1/E1/Program.cs
@@ -6,7 +6,43 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vector = new int[10];
+            int suma=0,menor=0,aux=0;
+            float prom=0;
+
+            Console.WriteLine("Ingrese 10 numeros\n");
+
+            for(int i=0;i<10;i++){
+                vector[i]=Int32.Parse(Console.ReadLine());
+            }
+
+            for(int i=0;i<10;i++){
+                suma += vector[i];
+                Console.WriteLine(vector[i]);
+            }
+
+            for(int i=0;i<=10;i++){
+                for(int j=0;j<9;j++){
+                    if(vector[j]<vector[j+1]){
+                        aux = vector[j];
+                        vector[j]=vector[j+1];
+                        vector[j+1]=aux;
+                    }
+                }
+            }
+
+            prom=suma/10;
+            menor=vector[9];
+            
+            Console.WriteLine("La suma de todos los numeros es: "+suma);
+            Console.WriteLine("El promedio de los numeros es: "+prom);
+            Console.WriteLine("El menor de los numeros es: "+menor);
+            
+            Console.WriteLine("El vector ordenado es: \n");
+            for(int i=0;i<10;i++){
+                Console.WriteLine(vector[i]);
+            }
+            
         }
     }
 }
