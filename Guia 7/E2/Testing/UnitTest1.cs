f7c5c5515b2d466ba7208c5e8749f49f3d8d5a52
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 21:38:40 -0300

    termine la parte A

--- Guia 7/E2/Testing/UnitTest1.cs
+++ Guia 7/E2/Testing/UnitTest1.cs
@@ -1,18 +1,66 @@
+using Ejercicio;
 using NUnit.Framework;
 
 namespace Testing
 {
     public class Tests
     {
+        Abuelo abuelo;
+        Necio necio;
+        Comun comun;
+        
+        Niño mary;
+        Niño jony;
+        //Niño n3;
+
         [SetUp]
         public void Setup()
         {
+            abuelo = new Abuelo();
+            necio = new Necio();
+            comun = new Comun();
+
+            mary = new Niño(new Maquillaje(),new Traje_Tierno(),16);
+            jony = new Niño(new Maquillaje(),new Traje_Terrorifico(),0);
+            //n3 = new Niño(new Maquillaje(),new Traje_Terrorifico());
         }
 
         [Test]
-        public void Test1()
+        public void TestLaCapacidadDeAsustarDeMaryEsDe5()
         {
-            Assert.Pass();
+            Assert.AreEqual(5,mary.Capacidad);
+        }
+        
+        [Test]
+        public void TestLaCapacidadDeAsustarDeJonyEsDe8()
+        {
+            Assert.AreEqual(8,jony.Capacidad);
+        }
+
+        [Test]
+        public void TestMaryAsustaUnAdultoComunYLuegoJonyLoIntentaPeroSuToleranciaEsMayorYNoConsigueNada()
+        {
+            mary.Asustar(comun);
+            jony.Asustar(comun);
+            
+            Assert.AreEqual(0,jony.Caramelos);
+        }
+
+        [Test]
+        public void TestMaryAsustaUnAbueloYLuegoJonyLoAsusta2VecesYConsigue4Caramelos()
+        {
+            mary.Asustar(abuelo);
+            jony.Asustar(abuelo);
+            jony.Asustar(abuelo);
+            
+            Assert.AreEqual(4,jony.Caramelos);
+        }
+
+        [Test]
+        public void TestJonyIntentaAsustarAUnNecioPeroNoObtieneNada()
+        {
+            jony.Asustar(necio);
+            Assert.AreEqual(0,jony.Caramelos);
         }
     }
 }
