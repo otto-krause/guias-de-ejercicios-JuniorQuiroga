7bd037c7d63ad50e0c38e386bc371cf967c3a721
JuniorQuiroga <quirogajunior25@gmail.com>
2020-11-09 14:29:11 -0300

    Termine el ejercicio 6

--- Guia 7/E5/Testing/UnitTest1.cs
+++ Guia 7/E5/Testing/UnitTest1.cs
@@ -8,35 +8,122 @@ namespace Testing
     public class Tests
     {
         Persona romelio;
-        Villano jose;
-        Villano v0;
-        Villano v1;
-        Villano v2;
-        Villano v3;
-        Pandilla pandilla;
-        Pandilla pandillaYJefe;
-
         Heroe endeavor;
+        Heroe oruMaito;
+        Heroe arturoMirodilla;
+
+        //villanos
+            Villano v0;
+            Villano v1;
+            Villano v2;
+            Villano v3;
+            Pandilla pandilla;
+            Pandilla pandillaYJefe;
+
+        //quirks
+            Quirk hellFlame;
+            Quirk aplastaPerritos;
+            Quirk horizon;
+            Quirk papaias;
+            Quirk hackerHTML;
+
+            Quirk oneForAll;
+            Quirk allForOne;
+
 
         [SetUp]
         public void Setup()
         {
-            endeavor = new Heroe(new Quirk("Hell Flame",300,false));
+            #region quirks
+                oneForAll = new Quirk("One For All",100,true);
+                allForOne = new Quirk("All For One",100,true);
 
-            v0 = new Villano(true,false,false,new Quirk("aplasta perritos",34,false));
-            v1 = new Villano(true,true,true,new Quirk("event horizon",10,true));
-            v2 = new Villano(true,false,false,new Quirk("papaias intensas",6,true));
+                hellFlame = new Quirk("Hell Flame",300,false);
+                aplastaPerritos = new Quirk("aplasta perritos",34,false);
+                horizon = new Quirk("event horizon",10,true);
+                papaias = new Quirk("papaias intensas",6,true);
+                hackerHTML = new Quirk("Hackear la nasa con HTML",600,true);
 
-            v3 = new Villano(true,true,true,new Quirk("Hackear la nasa con HTML",600,true));
-            pandilla = new Pandilla(new List<Villano>(){v0,v1,v2});
-            pandillaYJefe = new Pandilla(new List<Villano>(){v0,v1,v2,v3});
+            #endregion
+
+            #region villanos
+                v0 = new Villano(true,false,false,aplastaPerritos);
+                v1 = new Villano(true,true,true,horizon);
+                v2 = new Villano(true,false,false,papaias);
+
+                v3 = new Villano(true,true,true,hackerHTML);
+                pandilla = new Pandilla(new List<Villano>(){v0,v1,v2});
+                pandillaYJefe = new Pandilla(new List<Villano>(){v0,v1,v2,v3});
+            #endregion
+
+            romelio = new Persona(allForOne);
+            endeavor = new Heroe(hellFlame);
+            oruMaito = new Heroe(oneForAll);
+            arturoMirodilla = new Heroe(papaias);
         }
+
+    #region parte 2
+    
+        [Test]
+        public void TestOruMaitoTraspasaSuQuirk()
+        {
+            oruMaito.Traspasar(arturoMirodilla);
+            Assert.AreEqual("One For All",arturoMirodilla.suQuirk.Nombre);
+        }
+    
+        [Test]
+        public void TestOruTraspasaSuQuirkARomelioYEsteConservaElOriginalJuntoAlNuevo()
+        {
+            oruMaito.Traspasar(romelio);
+            Assert.IsTrue(oneForAll==romelio.suQuirk && allForOne == romelio.QuirkOriginal);
+        }
+
+        [Test]
+        public void TestUsaAllForOneYRobaLosQuirkDeOruEndeavorYArturo(){
+            romelio.Robar(endeavor);
+            romelio.Robar(oruMaito);
+            romelio.Robar(arturoMirodilla);
+            Assert.AreEqual(new List<Quirk>(){hellFlame,oneForAll,papaias},romelio.suQuirk.Robados);
+        }
+        
+        [Test]
+        public void TestRobaLosQuirkDeOruEndeavorYArturoYSuPOderEsDe(){
+            romelio.Robar(endeavor);
+            romelio.Robar(oruMaito);
+            romelio.Robar(arturoMirodilla);
+            Assert.AreEqual(4196,romelio.suQuirk.Poder());
+        }
+        
+        [Test]
+        public void TestRobaLosQuirkDeOruEndeavorYArturoYEsPeligroso(){
+            romelio.Robar(endeavor);
+            romelio.Robar(oruMaito);
+            romelio.Robar(arturoMirodilla);
+            Assert.IsTrue(romelio.suQuirk.Peligroso());
+        }
+        
+        [Test]
+        public void TetsTraspasaSuQuirkAEndeavorEsteAArturoYSuPoderEsDe1430()
+        {
+            oruMaito.Traspasar(endeavor);
+            endeavor.Traspasar(arturoMirodilla);
+            Assert.AreEqual(1430,arturoMirodilla.suQuirk.Poder());
+        }
+
+        [Test]
+        public void TestOneForAllEsPeligroso()
+        {
+            Assert.IsTrue(oruMaito.suQuirk.Peligroso());
+        }
+
+
+    #endregion
 
     #region ejercicio
         [Test]
         public void TestElQuirkDeEndeavorEsPeligroso()
         {
-            Assert.IsFalse(endeavor.suQuirk.EsPeligroso);
+            Assert.IsTrue(endeavor.suQuirk.Peligroso());
         }
         
         [Test]
@@ -63,36 +150,5 @@ namespace Testing
 
     #endregion
 
-    #region standard
-        [Test]
-        public void TestRomelioNoEsPeligroso()
-        {
-            romelio = new Persona(new Quirk("destripador de mares",15,true));
-            Assert.IsFalse(romelio.suQuirk.EsPeligroso);
-        }
-
-        [Test]
-        public void TestRomelioEsPeligroso()
-        {
-            romelio = new Persona(new Quirk("El todo poderosisimo destructor de huesos",960,false));
-            Assert.IsTrue(romelio.suQuirk.EsPeligroso);
-        }
-
-        [Test]
-        public void TestJoseAlgunaVezFueBueno()
-        {
-            jose = new Villano(true,false,true,new Quirk("aplasta perritos",10,false));
-            Assert.IsTrue(jose.FueBueno);
-        }
-
-        [Test]
-        public void TestEsMuyMalo()
-        {
-            jose = new Villano(true,true,true,new Quirk("aplasta perritos",10,false));
-            Assert.IsTrue(jose.MuyMalo);
-        }
-        
-    #endregion
-       
     }
 }
